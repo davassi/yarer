0af9f47bfe997d46dab00741a38ec7efd6c4218a
Gianluigi Davassi <gianluigi.davassi@klar.mx>
2023-08-10 15:49:15 +0300

    Added readline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 use std::io;
 use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 pub mod parser;
 pub mod rpn_resolver;
@@ -46,17 +48,36 @@ fn main() {
         println!("License MIT OR Apache-2.0");
     }
 
-    loop {
-        let mut input : String = String::new();
-        io::stdin().read_line(&mut input).expect("Input error.");
+    let mut rl = Editor::<()>::new();
 
-        if input.trim().is_empty() { continue; }
-        if input.trim().to_lowercase().eq("quit") { break; }
-        
-        let mut resolver : RpnResolver = RpnResolver::parse(&input);
-        let _ = resolver.resolve()
-            .and_then(|res: token::Number| {println!("{}", res); Ok(res)})
-            .or_else(|err| {println!("Error: {}", err); Err("Error")});
+    loop {
+        let readline = rl.readline("");
+
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                
+                if line.trim().is_empty() { continue; }
+                if line.trim().to_lowercase().eq("quit") { break; }
+                
+                let mut resolver : RpnResolver = RpnResolver::parse(&line);
+                let _ = resolver.resolve()
+                    .and_then(|res: token::Number| {println!("{}", res); Ok(res)})
+                    .or_else(|err| {println!("Error: {}", err); Err("Error")});
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("quit");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("quit");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
     }
 }
 
